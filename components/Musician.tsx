e1edcb4b7cd3efe996cb2438badb993ab1b6d55a
Hamed Sedighi <hamedsedighi@gmail.com>
2021-02-24 16:35:28 +0100

    :wrench: Render users on home page.

--- components/Musician.tsx
+++ components/Musician.tsx
@@ -1,3 +1,4 @@
+import Nextlink from 'next/link'
 import _ from 'lodash'
 import {
     SimpleGrid,
@@ -7,6 +8,7 @@ import {
     Avatar,
     Text,
     LinkBox,
+    Link,
     LinkOverlay,
     Stack,
     Button,
@@ -15,10 +17,38 @@ import {
 } from '@chakra-ui/react'
 
 export default function Musician({ data }) {
+    
     return (
-        <>
-            <p>salam</p>
-            {/* { data.map((i) => i)} */}
-        </>
+        <Stack
+        spacing={2}
+        flexDir='row'
+        >
+
+            {_.map(data, (i) => (
+                <Box
+                    key={i.uid}
+                    width={['400px', '200px', '100px', '150px']}
+                    p={3}
+                    minH={['150px', '170px', '200px', '100px']}
+                    bg='gray.900'
+                >
+                    <Nextlink href={`/${i.uid}`} passHref>
+                        <Link>
+                            <Stack >
+                                <Avatar
+                                    borderRadius={[20, 40, 20, 80]}
+                                    // h={'200px'}
+                                    size="xl"
+                                    name={i.name}
+                                    src={i.photoUrl}
+                                    objectFit='cover'
+                                />
+                                <Text fontSize={'xs'}>{i.name}</Text>
+                            </Stack>
+                        </Link>
+                    </Nextlink>
+                </Box>
+            ))}
+        </Stack>
     )
 }
