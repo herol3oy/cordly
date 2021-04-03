fe5940b70e2f10b24c01505d8a310bf8c0aefa05
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-03 17:41:18 +0200

    :tada: Home page phone prev carousel

---
+++ components/PhonePrevHome.tsx
@@ -0,0 +1,282 @@
+import NextLink from "next/link";
+import {
+    Heading,
+    Flex,
+    Text,
+    Button,
+    Avatar,
+    Box,
+    Badge,
+    TabList,
+    Tabs,
+    TabPanels,
+    TabPanel,
+    Tab,
+    Stack,
+    Link,
+    Wrap,
+    WrapItem,
+} from "@chakra-ui/react";
+
+const urls = [
+    { Youtube: "https://youtube.com" },
+    { Website: "https://google.com" },
+    { Spotify: "https://spotify.com" },
+    { dizzer: "https://dizzer.com" },
+    { Instagram: "https://instagram.com" },
+];
+
+export default function PhonePrevHome() {
+    return (
+        <>
+            <Box
+                borderColor={"black"}
+                width={["90vw", 'sm']}
+                borderWidth={[12, 15]}
+                borderRadius={65}
+                maxH={["600px", "580px", "580px", "700px "]}
+                minH={"700px"}
+                overflow="hidden"
+                textAlign="center"
+                // my={10}
+                pos={"relative"}
+                transform={'scale(0.8)'}
+            >
+                <Flex
+                    background={"gray"}
+                    backgroundImage={`url("https://is.gd/y55ADb")`}
+                    backgroundRepeat={"no-repeat"}
+                    backgroundSize={"cover"}
+                    backgroundPosition={'center'}
+                    height={"150px"}
+                    w={"sm"}
+                    pos="absolute"
+                    _after={{
+                        display: "inline-block",
+                        content: '""',
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                        width: "100%",
+                        height: "100%",
+                        background: `linear-gradient(to top, #fff 0%, rgba(255, 255, 255, 0) 100%)`,
+                    }}
+                />
+
+                <Avatar
+                    zIndex="3"
+                    mt={28}
+                    size="lg"
+                    name="profile picture"
+                    src={"https://is.gd/ELrhDq"}
+                />
+                <Heading as={"h3"} size={"md"} mt={2}>
+                    Morrie
+            </Heading>
+
+                <Flex mt={2} mb={5} justifyContent="center" alignItems="center">
+                    <Text fontWeight={"light"} textAlign="center">
+                        @morriemusician
+              </Text>
+
+                    <Badge variant="solid" colorScheme="green" ml={2}>
+                        PRO
+              </Badge>
+                </Flex>
+
+                <Tabs
+                    zIndex="2"
+                    pos="absolute"
+                    w="100%"
+                    isFitted
+                    align="center"
+                    variant="line"
+                    colorScheme="green"
+                >
+                    <TabList>
+                        <Tab>
+                            <Box>🔗</Box>
+                            <Text ml={3}>Links</Text>
+                        </Tab>
+
+                        <Tab>
+                            <Box>✍️</Box>
+                            <Text ml={3}>Bio</Text>
+                        </Tab>
+                    </TabList>
+
+                    <TabPanels>
+                        <TabPanel>
+                            <Stack>
+                                {urls?.map((i, idx) => (
+                                    <Button size="lg" key={idx} w={"100%"}>
+                                        <NextLink
+                                            href={Object.values(i)[0].toString()}
+                                            passHref
+                                        >
+                                            <Link isExternal>{Object.keys(i)[0].toString()}</Link>
+                                        </NextLink>
+                                    </Button>
+                                ))}
+                            </Stack>
+                        </TabPanel>
+
+                        <TabPanel>
+                            <Stack>
+                                <Text
+                                    color={"purple.400"}
+                                    fontWeight={600}
+                                    fontSize={"sm"}
+                                    // bg={useColorModeValue('purple.50', 'purple.900')}
+                                    bg={"purple.900"}
+                                    p={2}
+                                    alignSelf={"center"}
+                                    rounded={"md"}
+                                >
+                                    📍 Barcelona
+                    </Text>
+
+                                <Wrap justify={"center"}>
+                                    <WrapItem>
+                                        <Text
+                                            color={"gray.400"}
+                                            fontWeight={600}
+                                            fontSize={"sm"}
+                                            bg={"gray.900"}
+                                            p={2}
+                                            alignSelf={"center"}
+                                            rounded={"md"}
+                                        >
+                                            🎓 Self-studied
+                        </Text>
+                                    </WrapItem>
+
+                                    <WrapItem>
+                                        <Text
+                                            color={"gray.400"}
+                                            fontWeight={600}
+                                            fontSize={"sm"}
+                                            bg={"gray.900"}
+                                            p={2}
+                                            alignSelf={"center"}
+                                            rounded={"md"}
+                                        >
+                                            🟢 Collaboration
+                        </Text>
+                                    </WrapItem>
+
+                                    <WrapItem>
+                                        <Text
+                                            color={"gray.400"}
+                                            fontWeight={600}
+                                            fontSize={"sm"}
+                                            bg={"gray.900"}
+                                            p={2}
+                                            alignSelf={"center"}
+                                            rounded={"md"}
+                                        >
+                                            🟣 Teaching
+                        </Text>
+                                    </WrapItem>
+                                </Wrap>
+
+                                <Wrap justify={"center"}>
+                                    {"Piano"
+                                        ?.split(",")
+                                        .slice(0, 5)
+                                        .map((i, idx) => (
+                                            <WrapItem key={idx}>
+                                                <Text
+                                                    key={idx}
+                                                    color={"gray.400"}
+                                                    fontWeight={600}
+                                                    fontSize={"sm"}
+                                                    bg={"gray.900"}
+                                                    p={3}
+                                                    alignSelf={"flex-start"}
+                                                    rounded={"md"}
+                                                >
+                                                    💯 {i}
+                                                </Text>
+                                            </WrapItem>
+                                        ))}
+                                </Wrap>
+
+                                <Wrap justify={"center"}>
+                                    {"Rock"
+                                        ?.split(",")
+                                        .slice(0, 3)
+                                        .map((i, idx) => (
+                                            <WrapItem key={idx}>
+                                                <Text
+                                                    key={idx}
+                                                    color={"gray.400"}
+                                                    fontWeight={600}
+                                                    fontSize={"sm"}
+                                                    bg={"gray.900"}
+                                                    p={3}
+                                                    alignSelf={"flex-start"}
+                                                    rounded={"md"}
+                                                >
+                                                    🎵 {i}
+                                                </Text>
+                                            </WrapItem>
+                                        ))}
+                                </Wrap>
+
+                                <Wrap justify={"center"}>
+                                    {"Metallica"
+                                        ?.split(",")
+                                        .slice(0, 5)
+                                        .map((i, idx) => (
+                                            <WrapItem key={idx}>
+                                                <Text
+                                                    key={idx}
+                                                    color={"gray.400"}
+                                                    fontWeight={600}
+                                                    fontSize={"sm"}
+                                                    bg={"gray.900"}
+                                                    p={3}
+                                                    alignSelf={"center"}
+                                                    rounded={"md"}
+                                                >
+                                                    🔥 {i}
+                                                </Text>
+                                            </WrapItem>
+                                        ))}
+                                </Wrap>
+                            </Stack>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+            <Flex
+                alignItems='center'
+                borderRadius={50}
+                bg="gray.700"
+                px={8}
+                py={4}
+                transform={'scale(0.7)'}
+                mt={['-10', '-18']}
+            >
+                <Text
+                    d={'flex'}
+                    alignItems='center'
+                    justifyContent='center'
+                    bg='green.400'
+                    w={10}
+                    h={10}
+                    borderRadius={64}
+                    p={2}
+                    fontWeight={'bold'}
+                >C</Text>
+                <Flex>
+                    <Text ml={2} textColor="green.300" fontWeight='light' fontSize={"xx-large"}>
+                        cord.ly/
+              </Text>
+                    <Text textColor="white" fontWeight="light" fontSize={"xx-large"}>demah</Text>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-import NextLink from "next/link";
 import { firestore } from "../lib/firebase";
-import Musician from "../components/Musician";
 import SvgListeningMusic from "../components/SvgListeningMusic";
-import SvgYoga from "../components/SvgYoga";
+import PhonePrevHome from "../components/PhonePrevHome";
 import SvgHeart from "../components/SvgHeart";
 import SvgShape1 from "../components/SvgShape1";
 import Footer from "../components/Footer";
@@ -15,29 +13,10 @@ import {
   Container,
   SimpleGrid,
   Text,
-  Image,
   Button,
   Avatar,
   Box,
-  Badge,
-  TabList,
-  Tabs,
-  TabPanels,
-  TabPanel,
-  Tab,
-  Stack,
-  Link,
-  Wrap,
-  WrapItem,
 } from "@chakra-ui/react";
-
-const urls = [
-  { Youtube: "https://youtube.com" },
-  { Website: "https://google.com" },
-  { Spotify: "https://spotify.com" },
-  { dizzer: "https://dizzer.com" },
-  { Instagram: "https://instagram.com" },
-];
 
 const responsive = {
   superLargeDesktop: {
@@ -62,6 +41,25 @@ const responsive = {
   },
 };
 
+const responsivePhonePrev = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
 export default function Home({ musicians }) {
   return (
     <Container h="100vh" maxW="container.xl">
@@ -69,7 +67,7 @@ export default function Home({ musicians }) {
         <Flex
           flexDirection={"column"}
           alignItems={["center", "start"]}
-          justify="center"
+          justifyContent="center"
         // my={10}
         >
           <SvgShape1 style={{ position: 'absolute', zIndex: -1 }} />
@@ -91,260 +89,28 @@ export default function Home({ musicians }) {
           </Button>
         </Flex>
 
-        <Flex
-          justify="center"
-          flexDir="column"
-          alignItems="center"
-        >
-          <Box
-            borderColor={"gray.300"}
-            width={["90vw", 'sm']}
-            borderWidth={[12, 15]}
-            borderRadius={65}
-            maxH={["600px", "580px", "580px", "700px "]}
-            minH={"700px"}
-            overflow="hidden"
-            textAlign="center"
-            // my={10}
-            pos={"relative"}
-            transform={'scale(0.8)'}
-          >
-            <Flex
-              background={"gray"}
-              backgroundImage={`url("https://is.gd/y55ADb")`}
-              backgroundRepeat={"no-repeat"}
-              backgroundSize={"cover"}
-              backgroundPosition={'center'}
-              height={"150px"}
-              w={"sm"}
-              pos="absolute"
-              _after={{
-                display: "inline-block",
-                content: '""',
-                position: "absolute",
-                left: 0,
-                top: 0,
-                width: "100%",
-                height: "100%",
-                background: `linear-gradient(to top, #fff 0%, rgba(255, 255, 255, 0) 100%)`,
-              }}
-            />
+        <Carousel
+          ssr={true}
+          infinite={true}
+          showDots={false}
+          swipeable={true}
+          autoPlay={true}
+          // customTransition="all 1"
+          // transitionDuration={3000}
+          autoPlaySpeed={4000}
+          partialVisbile
+          removeArrowOnDeviceType={['superLargeDesktop', 'desktop', "tablet", "mobile"]}
+          responsive={responsivePhonePrev}>
 
-            <Avatar
-              zIndex="3"
-              mt={28}
-              size="lg"
-              name="profile picture"
-              src={"https://is.gd/ELrhDq"}
-            />
-            <Heading as={"h3"} size={"md"} mt={2}>
-              Morrie
-            </Heading>
 
-            <Flex mt={2} mb={5} justifyContent="center" alignItems="center">
-              <Text fontWeight={"light"} textAlign="center">
-                @morriemusician
-              </Text>
+          {Array.from(Array(2).keys()).map(() => (
+            <Flex flexDirection='column' justify='center' alignItems='center'>
 
-              <Badge variant="solid" colorScheme="green" ml={2}>
-                PRO
-              </Badge>
+              <PhonePrevHome />
             </Flex>
+          ))}
 
-            <Tabs
-              zIndex="2"
-              pos="absolute"
-              w="100%"
-              isFitted
-              align="center"
-              variant="line"
-              colorScheme="green"
-            >
-              <TabList>
-                <Tab>
-                  <Box>🔗</Box>
-                  <Text ml={3}>Links</Text>
-                </Tab>
-
-                <Tab>
-                  <Box>✍️</Box>
-                  <Text ml={3}>Bio</Text>
-                </Tab>
-              </TabList>
-
-              <TabPanels>
-                <TabPanel>
-                  <Stack>
-                    {urls?.map((i, idx) => (
-                      <Button size="lg" key={idx} w={"100%"}>
-                        <NextLink
-                          href={Object.values(i)[0].toString()}
-                          passHref
-                        >
-                          <Link isExternal>{Object.keys(i)[0].toString()}</Link>
-                        </NextLink>
-                      </Button>
-                    ))}
-                  </Stack>
-                </TabPanel>
-
-                <TabPanel>
-                  <Stack>
-                    <Text
-                      color={"purple.400"}
-                      fontWeight={600}
-                      fontSize={"sm"}
-                      // bg={useColorModeValue('purple.50', 'purple.900')}
-                      bg={"purple.900"}
-                      p={2}
-                      alignSelf={"center"}
-                      rounded={"md"}
-                    >
-                      📍 Barcelona
-                    </Text>
-
-                    <Wrap justify={"center"}>
-                      <WrapItem>
-                        <Text
-                          color={"gray.400"}
-                          fontWeight={600}
-                          fontSize={"sm"}
-                          bg={"gray.900"}
-                          p={2}
-                          alignSelf={"center"}
-                          rounded={"md"}
-                        >
-                          🎓 Self-studied
-                        </Text>
-                      </WrapItem>
-
-                      <WrapItem>
-                        <Text
-                          color={"gray.400"}
-                          fontWeight={600}
-                          fontSize={"sm"}
-                          bg={"gray.900"}
-                          p={2}
-                          alignSelf={"center"}
-                          rounded={"md"}
-                        >
-                          🟢 Collaboration
-                        </Text>
-                      </WrapItem>
-
-                      <WrapItem>
-                        <Text
-                          color={"gray.400"}
-                          fontWeight={600}
-                          fontSize={"sm"}
-                          bg={"gray.900"}
-                          p={2}
-                          alignSelf={"center"}
-                          rounded={"md"}
-                        >
-                          🟣 Teaching
-                        </Text>
-                      </WrapItem>
-                    </Wrap>
-
-                    <Wrap justify={"center"}>
-                      {"Piano"
-                        ?.split(",")
-                        .slice(0, 5)
-                        .map((i, idx) => (
-                          <WrapItem key={idx}>
-                            <Text
-                              key={idx}
-                              color={"gray.400"}
-                              fontWeight={600}
-                              fontSize={"sm"}
-                              bg={"gray.900"}
-                              p={3}
-                              alignSelf={"flex-start"}
-                              rounded={"md"}
-                            >
-                              💯 {i}
-                            </Text>
-                          </WrapItem>
-                        ))}
-                    </Wrap>
-
-                    <Wrap justify={"center"}>
-                      {"Rock"
-                        ?.split(",")
-                        .slice(0, 3)
-                        .map((i, idx) => (
-                          <WrapItem key={idx}>
-                            <Text
-                              key={idx}
-                              color={"gray.400"}
-                              fontWeight={600}
-                              fontSize={"sm"}
-                              bg={"gray.900"}
-                              p={3}
-                              alignSelf={"flex-start"}
-                              rounded={"md"}
-                            >
-                              🎵 {i}
-                            </Text>
-                          </WrapItem>
-                        ))}
-                    </Wrap>
-
-                    <Wrap justify={"center"}>
-                      {"Metallica"
-                        ?.split(",")
-                        .slice(0, 5)
-                        .map((i, idx) => (
-                          <WrapItem key={idx}>
-                            <Text
-                              key={idx}
-                              color={"gray.400"}
-                              fontWeight={600}
-                              fontSize={"sm"}
-                              bg={"gray.900"}
-                              p={3}
-                              alignSelf={"center"}
-                              rounded={"md"}
-                            >
-                              🔥 {i}
-                            </Text>
-                          </WrapItem>
-                        ))}
-                    </Wrap>
-                  </Stack>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          </Box>
-
-          <Flex
-            alignItems='center'
-            justifyContent='center'
-            borderRadius={50}
-            backgroundColor="gray.700"
-            px={8}
-            py={4}
-          >
-            <Text
-              d={'flex'}
-              alignItems='center'
-              justifyContent='center'
-              bg='green.400'
-              w={10}
-              h={10}
-              borderRadius={64}
-              p={2}
-              fontWeight={'bold'}
-            >C</Text>
-            <Flex>
-              <Text ml={2} textColor="green.300" fontWeight='light' fontSize={"xx-large"}>
-                cord.ly/
-              </Text>
-              <Text textColor="white" fontWeight="light" fontSize={"xx-large"}>herol3oy</Text>
-            </Flex>
-          </Flex>
-        </Flex>
+        </Carousel>
       </SimpleGrid>
 
       <Flex
@@ -374,9 +140,8 @@ export default function Home({ musicians }) {
           partialVisbile
           responsive={responsive}
           removeArrowOnDeviceType={["tablet", "mobile"]}
-
         >
-          {Array.from(Array(10).keys()).map((item) => (
+          {Array.from(Array(10).keys()).map(() => (
             <>
               <Flex
                 rounded="lg"
