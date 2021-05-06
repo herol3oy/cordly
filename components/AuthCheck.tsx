de4d9065fca7c9d62e1895dfc61c7ebd4275bcd7
Hamed Sedighi <hamedsedighi@gmail.com>
2021-05-06 15:47:25 +0200

    :package: Redirect on no user on dashboard

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "../lib/context";
 import { auth } from "../lib/firebase";
 import {
@@ -10,37 +10,46 @@ import {
   Container,
   Text,
 } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export default function AuthCheck(props) {
   const { user } = useContext(UserContext);
-  const isUserVerified = auth.currentUser?.emailVerified;
+  // const isUserVerified = auth.currentUser?.emailVerified;
 
-  return user && isUserVerified
+  return user
+    // && isUserVerified
     ? props.children
-    : props.fallback || <SkeletonContent />;
+    : props.fallback || <SkeletonContent user={user} />;
 }
 
-const SkeletonContent = () => {
+const SkeletonContent = ({ user }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/')
+    }
+  }, []);
+
   return (
     <Stack
       display="flex"
       flexDirection="row"
       alignContent="center"
-      alignItems="center"
       justifyContent="center"
-    >
-      <Container>
-        <Text my={10} fontWeight="bold" fontSize="xx-large" textAlign="center">
-          Please verify your account by clicking on the link that has been sent
-          to your email.
-        </Text>
+      height='100vh'
+      width='100vw'>
+      <Container mt='10'>
         <SkeletonCircle size="12" m="auto" />
         <Box padding="6" boxShadow="lg">
           <Skeleton height="20px" />
           <Skeleton height="20px" />
           <Skeleton height="20px" />
         </Box>
-        <SkeletonText mt="4" noOfLines={4} spacing="4" />
+        <SkeletonText
+          mt="4"
+          noOfLines={4}
+          spacing="4" />
       </Container>
     </Stack>
   );
